66b7dac1d76d8ae2a9f57a41bfc93e3b1ea5aa9a
Anton Koekemoer <akoekemo@amazon.com>
2026-01-16 16:43:53 -0500

    Refactor cprintf into modular include files
    - Split cprintf into separate include files for better maintainability
    - Created cprintf-ansi.inc for ANSI escape code handling
    - Created cprintf-color-aliases.inc for color name mappings
    - Renamed cprintf.colorize-2.0 to cprintf-colorize.inc
    - Renamed cprintf.color-codes.inc to cprintf-opt-color-codes.inc
    - Moved optional features to cprintf-opt-*.inc files:
      - cprintf-opt-check-color-support.inc
      - cprintf-opt-supported-tags.inc
    - Updated main cprintf script to source include files
    - Updated install.sh to handle new file structure
    - Removed deprecated files (cprintf.colorize-1.0, cprintf.color-codes.inc, cprintf.supported-tags.inc)

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 set -e
 
 REPO="antonjk/bash-cprintf"
-INSTALL_DIR="/usr/local/share/cprintf"
-BIN_DIR="/usr/local/bin"
-MAN_DIR="/usr/share/man/man1"
-
 echo "Installing bash-cprintf..."
 
 # Download latest release
@@ -19,24 +15,4 @@ unzip -q cprintf.zip
 cd bash-cprintf-main
 
 # Install files
-sudo mkdir -p "$INSTALL_DIR"
-sudo cp -r * "$INSTALL_DIR/"
-sudo chmod +x "$INSTALL_DIR/cprintf"
-
-# Create symlink
-sudo ln -sf "$INSTALL_DIR/cprintf" "$BIN_DIR/cprintf"
-
-# Install man page
-if [ -f "doc/cprintf.1" ]; then
-    sudo mkdir -p "$MAN_DIR"
-    sudo cp doc/cprintf.1 "$MAN_DIR/cprintf.1"
-    sudo chmod 644 "$MAN_DIR/cprintf.1"
-    echo "Man page installed to $MAN_DIR/cprintf.1"
-fi
-
-# Cleanup
-rm -rf "$TEMP_DIR"
-
-echo "bash-cprintf installed successfully!"
-echo "Usage: cprintf '<fg:red>Hello</fg> <b>World</b>'"
-echo "See 'man cprintf' for complete documentation"
+sudo make install
